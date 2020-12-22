1ed63d5c62540c5952c3f74dc604ac84f1fcce23
Liubire <zlata744877@gmail.com>
2020-12-22 14:12:13 +0200

    deleted file and arranged chenges

--- tests/test_autotest_check_yashaka_selene.py
+++ tests/test_autotest_check_yashaka_selene.py
@@ -1,5 +1,12 @@
-from autotest_check_yashaka_selene import __version__
+from selene.support.conditions import have
+from selene.support.shared import browser
+from selene.support.shared.jquery_style import s, ss
 
 
-def test_version():
-    assert __version__ == '0.1.0'
+def test_search():
+    browser.open('https://www.ecosia.org/')
+
+    s('[name="q"]').type('yashaka selene').press_enter()
+    ss('.result').first.click()
+
+    ss('[href="/yashaka/selene"]').should(have.size(3))
